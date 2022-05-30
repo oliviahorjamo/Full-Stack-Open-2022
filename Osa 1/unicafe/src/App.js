48fccccec57877ac044032e9d66d8e5e65919ed1
Olivia Horjamo <olivia.horjamo@gmail.com>
2022-05-30 16:53:11 +0300

    Complete exercise 1.7

--- Osa 1/unicafe/src/App.js
+++ Osa 1/unicafe/src/App.js
@@ -1,23 +1,39 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [count, setCount] = useState(0)
+  const [sum, setSum] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive_share, setSharePositive] = useState(0)
+  const [positives_count, setCountPositive] = useState(0)
+
+  useEffect(() => {
+    setAverage(sum / count)
+    setSharePositive(positives_count / count * 100 + "%")
+  })
+
 
   const handleGoodClick = () => {
     setGood(good + 1)
+    setCount(count + 1)
+    setSum(sum + 1)
+    setCountPositive(positives_count + 1)
   }
-
 
   const handleNeutralClick = () => {
     setNeutral(neutral + 1)
+    setCount(count + 1)
   }
 
 
   const handleBadClick = () => {
     setBad(bad + 1)
+    setCount(count + 1)
+    setSum(sum - 1)
   }
 
   return (
@@ -30,6 +46,9 @@ const App = () => {
       <p>good: {good}</p>
       <p>neutral: {neutral}</p>
       <p>bad: {bad}</p>
+      <p>count: {count}</p>
+      <p>average: {average}</p>
+      <p>positive: {positive_share}</p>
     </div>
   )
 }
