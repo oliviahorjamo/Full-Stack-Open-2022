9bdfa678465e47769ea150fc19569ac490af9ed3
Olivia Horjamo <olivia.horjamo@gmail.com>
2022-06-02 20:09:11 +0300

    Complete exercise 2.11

--- Osa 2/puhelinluettelo/src/App.js
+++ Osa 2/puhelinluettelo/src/App.js
@@ -1,19 +1,25 @@
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Filter from './filter_form'
 import FilteredList from './filtered_list'
+import axios from "axios"
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
-
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState("")
   const [newFilter, setNewFilter] = useState("")
+
+  useEffect(() => {
+    axios.
+    get("http://localhost:3001/persons")
+    .then(response => {
+      console.log("promise fulfilled")
+      console.log(response.data)
+      setPersons(response.data)
+    })
+  }, [])
+
 
   const handleNewName = (event) => {
     const name = event.target.value
@@ -43,14 +49,6 @@ const App = () => {
   const handleFilterChange = (event) => {
     const filter = event.target.value
     setNewFilter(filter)
-  }
-
-  const filterList = () => {
-    const list = persons
-    const filter_str = newFilter
-    return list.filter(function(person) {
-      return person.name.toUpperCase().indexOf(filter_str.toUpperCase()) !== -1
-    })
   }
 
   return (
