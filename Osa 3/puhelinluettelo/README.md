36f8582780edfb79971f6cc2ed032c3f10c87ec8
Olivia Horjamo <olivia.horjamo@gmail.com>
2022-09-07 18:11:25 +0300

    Complete exercises 3.9-3.11

---
+++ Osa 3/puhelinluettelo/README.md
@@ -0,0 +1,3 @@
+The app is built on Heroku at:
+
+https://floating-gorge-82555.herokuapp.com/api/persons
--- Osa 3/puhelinluettelo/index.js
+++ Osa 3/puhelinluettelo/index.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+const cors = require('cors')
+
+app.use(cors())
 
 app.use(express.json())
+
+app.use(express.static('build'))
 
 morgan.token('data', function(req, res) {
     return JSON.stringify(req.body)
@@ -108,7 +113,7 @@ const unknownEndpoint = (request, response) => {
   app.use(unknownEndpoint)
   
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
