652e96ac3a9cd6d688ef275e057814821eeaf719
Olivia Horjamo <olivia.horjamo@gmail.com>
2023-01-09 20:34:38 +0200

    Exercise 3.22

--- Osa 3/puhelinluettelo/index.js
+++ Osa 3/puhelinluettelo/index.js
@@ -12,8 +12,8 @@ app.use(express.json())
 
 app.use(express.static('build'))
 
-morgan.token('data', function(req, res) {
-    return JSON.stringify(req.body)
+morgan.token('data', function(req) {
+  return JSON.stringify(req.body)
 })
 
 app.use(morgan(':method :url :status :res[content-length] :response-time ms :data'))
@@ -23,97 +23,97 @@ app.get('/', (req, res) => {
 })
 
 app.get('/info', (req, res) => {
-    Person.find({}).then(persons => {
-        const number = persons.length
-        const now = new Date()
+  Person.find({}).then(persons => {
+    const number = persons.length
+    const now = new Date()
 
-        res.send(`<p>Phonebook has info for ${number} people
+    res.send(`<p>Phonebook has info for ${number} people
         <br>${now}<p>`)
-    })
   })
+})
 
 app.get('/api/persons', (req, res) => {
-    Person.find({})
+  Person.find({})
     .then(persons => {
-        res.json(persons)
+      res.json(persons)
     })
 })
 
 app.get('/api/persons/:id', (req, res, next) => {
-    Person.findById(req.params.id)
+  Person.findById(req.params.id)
     .then(person => {
-        if (person) {
-            res.json(person)
-        } else {
-            res.status(404).end()
-        }
+      if (person) {
+        res.json(person)
+      } else {
+        res.status(404).end()
+      }
     })
     .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (req, res, next) => {
-    console.log('deletessä')
-    Person.findByIdAndRemove(req.params.id)
-    .then(result => {
-        res.status(204).end()
-    })
+  console.log('deletessä')
+  Person.findByIdAndRemove(req.params.id)
+    .then(
+      res.status(204).end()
+    )
     .catch(error => next(error))
-  })
+})
 
 
 app.put('/api/persons/:id', (req, res, next) => {
-    const {name, number} = req.body
-    
-    Person.findByIdAndUpdate(
-        req.params.id,
-        { name, number },
-        { new: true, runValidators: true, context: 'query '}
-    )
+  const { name, number } = req.body
+
+  Person.findByIdAndUpdate(
+    req.params.id,
+    { name, number },
+    { new: true, runValidators: true, context: 'query ' }
+  )
     .then(updatedNumber => {
-        res.json(updatedNumber)
+      res.json(updatedNumber)
     })
     .catch(error => next(error))
 })
 
 app.post('/api/persons', (req, res, next) => {
-    const body = req.body
+  const body = req.body
 
-    Person.find({}).then(persons => {
-        const names = persons.map(p => p.name)
+  Person.find({}).then(persons => {
+    const names = persons.map(p => p.name)
 
-        if (names.includes(body.name)) {
-            return res.status(400).json({
-                error: 'name already in the phonebook'
-            })
-        } else {
-            newPerson = Person({
-                name: body.name,
-                number: body.number
-            })
-        
-            newPerson.save().then(savedPerson => {
-                res.json(savedPerson)
-            })
-            .catch(error => next(error))
-        }
-    })
+    if (names.includes(body.name)) {
+      return res.status(400).json({
+        error: 'name already in the phonebook'
+      })
+    } else {
+      const newPerson = Person({
+        name: body.name,
+        number: body.number
+      })
+
+      newPerson.save().then(savedPerson => {
+        res.json(savedPerson)
+      })
+        .catch(error => next(error))
+    }
+  })
 
 })
 
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
-  
-  app.use(unknownEndpoint)
-  
-  const errorHandler = (error, request, response, next) => {
-    console.log(error.message)
-    if (error.name === 'CastError') {
-        return response.status(400).send({error: 'malformatted id'})
-    } else if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message})
-    }
-    next(Error)
+  next(Error)
 }
 
 app.use(errorHandler)
