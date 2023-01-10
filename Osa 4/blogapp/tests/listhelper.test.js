29b19747849f4b14ff8a899a88bd75e8430ae9cb
Olivia Horjamo <olivia.horjamo@gmail.com>
2023-01-10 14:00:46 +0200

    Exercises 4.4 and 4.5

--- Osa 4/blogapp/tests/listhelper.test.js
+++ Osa 4/blogapp/tests/listhelper.test.js
@@ -1,8 +1,75 @@
 const listHelper = require('../utils/list_helper.js')
 
-test('dummy returns one', () => {
-  const blogs = []
+listWithOneBlog = [
+  {
+  _id: '5a422aa71b54a676234d17f8',
+  title: 'Go To Statement Considered Harmful',
+  author: 'Edsger W. Dijkstra',
+  url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+  likes: 5,
+  __v: 0
+}
+]
 
-  const result = listHelper.dummy(blogs)
-  expect(result).toBe(1)
+listWithThreeBlogs = [
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0
+  },
+  {
+    _id: '5a422aa71b54a676234d17f9',
+    title: 'testtitle 2',
+    author: 'author 2',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 3,
+    __v: 0
+  },
+  {
+    _id: '5a422aa71b54a676234d17f10',
+    title: 'testtitle 3',
+    author: 'author 3',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0
+  } 
+]
+
+describe('total likes', () => {
+
+  test('when list has only one blog, equals, the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    expect(result).toBe(5)
+  })
+
+  test('of empty list is zero', () => {
+    const result = listHelper.totalLikes([])
+    expect(result).toBe(0)
+  })
+
+  test('of list with three entires correct', () => {
+    const result = listHelper.totalLikes(listWithThreeBlogs)
+    expect(result).toBe(13)
+  })
+})
+
+describe('object with max likes', () => {
+
+  test('is itself in a list of one object', () => {
+    const result = listHelper.favoriteBlog(listWithOneBlog)
+    expect(result).toEqual(listWithOneBlog[0])
+  })
+
+  test('is correct in a long list of objects', () => {
+    const result = listHelper.favoriteBlog(listWithThreeBlogs)
+    expect(result).toEqual(listWithThreeBlogs[listWithThreeBlogs.length -1])
+  })
+
+  test('is null when given an empty list', () => {
+    const result = listHelper.favoriteBlog([])
+    expect(result).toEqual(null)
+  })
 })
--- Osa 4/blogapp/utils/list_helper.js
+++ Osa 4/blogapp/utils/list_helper.js
@@ -1,7 +1,22 @@
-const dummy = (blogs) => {
-  return 1
+
+const totalLikes = (blogs) => {
+  const reducer = (sum, item) => {
+    return sum + item.likes
+  }
+
+  return blogs.reduce(reducer, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  if (blogs.length !== 0) {
+    max = blogs.reduce((prev, current) => (prev.y > current.y) ? prev : current)
+  } else {
+    max = null
+  }
+  return max
 }
 
 module.exports = {
-  dummy
+  totalLikes,
+  favoriteBlog
 }
