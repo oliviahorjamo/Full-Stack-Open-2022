5778a4318b09f337c3487de294c11fdc8b522cf8
Olivia Horjamo <olivia.horjamo@gmail.com>
2023-01-10 12:56:26 +0200

    Exercises 4.1 and 4.2

---
+++ Osa 4/blogapp/controllers/blogs.js
@@ -0,0 +1,23 @@
+const blogRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogRouter.get('/api/blogs', (request, response) => {
+  console.log('api blogs getiss')
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+blogRouter.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+module.exports = blogRouter
