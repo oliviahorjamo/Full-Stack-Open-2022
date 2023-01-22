1c5bdcf99abbea278ad89d6c521dee5b47a70000
Olivia Horjamo <olivia.horjamo@gmail.com>
2023-01-22 22:07:10 +0200

    Exercise 5.7

--- Osa 5/bloglist-frontend/src/components/Blog.js
+++ Osa 5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,40 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+import Togglable from "./Togglable"
+
+const Blog = ({ blog }) => {
+  //const [showBlog, setVisible] = useState(false)
+
+  console.log(blog)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const additionalInformation = () => (
+    <div>
+      <p>{blog.url}</p>
+      <p>likes {blog.likes}</p>
+      <button>like</button>
+      <p>{blog.user.name}</p>
+    </div>
+  )
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}
+      <Togglable buttonlabel='view'>
+        {additionalInformation()}
+      </Togglable>
+
+    </div>  
+
+  )
+
+}
 
 export default Blog
