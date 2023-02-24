9314b21a49b9e1d3dcf408e293be07cf67e98cc3
Olivia Horjamo <olivia.horjamo@gmail.com>
2023-02-24 02:10:50 +0200

    Exercise 6.19

--- Osa 6/redux-anecdotes/src/components/Notification.js
+++ Osa 6/redux-anecdotes/src/components/Notification.js
@@ -1,18 +1,50 @@
-import { useSelector } from "react-redux"
+import { connect } from "react-redux"
 
-const Notification = () => {
-  const notification = useSelector(state => state.notification)
+const FindStyle = (notification) => {
+  if (!notification) {
+    return {
+      visible: 'null'
+    }
+  } else {
+    return {
+      border: 'solid',
+      padding: 10,
+      borderWidth: 1
+    }
+  }
+}
 
+const Notification = (props) => {
+  //const notification = useSelector(state => state.notification)
+
+  /*
   const style = {
     border: 'solid',
     padding: 10,
     borderWidth: 1
   }
+
+  if (!props.state) {
+    console.log(props.state)
+    
+  }
+  */
+
+  const style = FindStyle(props.notification)
+
   return (
     <div style={style}>
-      <p>{notification}</p>
+      <p>{props.notification}</p>
     </div>
   )
 }
 
-export default Notification
+const mapStateToProps = (state) => {
+  return {
+    notification: state.notification
+  }
+}
+
+const ConnectedNotification = connect(mapStateToProps)(Notification)
+
+export default ConnectedNotification
