2f58de89bc48abfc30f49425896ae4bcccf100b2
Olivia Horjamo <olivia.horjamo@gmail.com>
2023-06-09 21:55:16 +0300

    Remove hardcoded backend url

--- Osa 12/part12-containers-applications/todo-app/todo-frontend/dev.Dockerfile
+++ Osa 12/part12-containers-applications/todo-app/todo-frontend/dev.Dockerfile
@@ -4,6 +4,8 @@ WORKDIR /usr/src/app
 
 COPY . .
 
+ENV REACT_APP_BACKEND_URL="http://localhost:3000"
+
 RUN npm install
 
 CMD ["npm", "start"]
--- Osa 12/part12-containers-applications/todo-app/todo-frontend/src/util/apiClient.js
+++ Osa 12/part12-containers-applications/todo-app/todo-frontend/src/util/apiClient.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  //baseURL: process.env.REACT_APP_BACKEND_URL,
-  baseURL: 'http://localhost:3000'
+  baseURL: process.env.REACT_APP_BACKEND_URL,
+  //baseURL: 'http://localhost:3000'
 })
 
 export default apiClient
