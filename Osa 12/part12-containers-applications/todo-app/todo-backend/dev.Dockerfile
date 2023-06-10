97158be60d305c5bae6fe199d467a2b42c58e4bb
Olivia Horjamo <olivia.horjamo@gmail.com>
2023-06-10 19:18:15 +0300

    Exercises 12.17 and 12.18

--- Osa 12/part12-containers-applications/todo-app/todo-backend/dev.Dockerfile
+++ Osa 12/part12-containers-applications/todo-app/todo-backend/dev.Dockerfile
@@ -9,8 +9,7 @@ RUN npm install
 
 EXPOSE 3000
 
-#ENV MONGO_URL=mongodb://the_username:the_password@localhost:27017/the_database
-#ENV REDIS_URL=redis://redis:6379
-    
+ENV MONGO_URL='mongodb://the_username:the_password@mongo:27017/the_database'
+ENV REDIS_URL='redis://redis:6379'
 
 CMD ["npm", "run", "dev"]
--- Osa 12/part12-containers-applications/todo-app/todo-backend/util/config.js
+++ Osa 12/part12-containers-applications/todo-app/todo-backend/util/config.js
@@ -1,10 +1,14 @@
 require('dotenv').config()
+// note that these should not be locally as these are
+// the urls required by docker
+// however, for some reason setting the env variables in .env
+// and dev.Dockerfile don't work
 const MONGO_URL = 'mongodb://the_username:the_password@mongo:27017/the_database'
 const REDIS_URL = 'redis://redis:6379'
 
 module.exports = {
-  MONGO_URL,//: 'mongodb://the_username:the_password@localhost:3456/the_database',
-  REDIS_URL//: '//localhost:6378'
+  MONGO_URL,//: process.env.MONGO_URL,//: 'mongodb://the_username:the_password@localhost:3456/the_database',
+  REDIS_URL//: process.env.REDIS_URL//: '//localhost:6378'
 }
 
 // enable the development of the todo app backend while it's running inside a container
