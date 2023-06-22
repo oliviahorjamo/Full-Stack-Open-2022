f49ffb46e341f468a4c7822667d2f180a0fa8e0c
Olivia Horjamo <olivia.horjamo@gmail.com>
2023-06-23 01:11:53 +0300

    Exercises 22.19-22.22

--- Osa 12/part12-containers-applications/todo-app/todo-frontend/src/Todos/Todo.js
+++ Osa 12/part12-containers-applications/todo-app/todo-frontend/src/Todos/Todo.js
@@ -32,7 +32,7 @@ const Todo = ({ todo, deleteTodo, completeTodo }) => {
     <>
           <div style={{ display: 'flex', justifyContent: 'space-between', maxWidth: '70%', margin: 'auto' }}>
             <span>
-              {todo.text} 
+              {todo.text}
             </span>
             {todo.done ? doneInfo : notDoneInfo}
           </div>
--- Osa 12/part12-containers-applications/todo-app/todo-frontend/src/util/apiClient.js
+++ Osa 12/part12-containers-applications/todo-app/todo-frontend/src/util/apiClient.js
@@ -2,7 +2,6 @@ import axios from 'axios'
 
 const apiClient = axios.create({
   baseURL: process.env.REACT_APP_BACKEND_URL,
-  //baseURL: 'http://localhost:3000'
 })
 
 export default apiClient
